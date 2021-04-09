1cc39bfdb3297f6a000a9b4f5ce3c7bfe4d731f5
Thomas Gredig <thomas.gredig@csulb.edu>
2021-04-09 15:51:25 -0700

    update code

---
+++ NSF-PendingSupport-Extractor.R
@@ -0,0 +1,53 @@
+# Tabulizes NSF Current and Pending Support from PDF files
+# NOTE: not efficient code; just a hack
+#
+
+# CONFIGURE THIS:
+# ===============
+# set path for PDF proposal PDFs
+path = '.'
+OUTPUT.file = file.path(path, 'output.csv')
+
+# start analysis
+library(pdftools)
+
+# find all the proposals
+file.list = dir(path, pattern='^NSF.*pdf$')
+
+# create output
+r = data.frame()
+for(f in file.list) {
+  filename = file.path(path,f)
+  print(filename)
+  t = pdf_text(filename) %>% readr::read_lines()
+  m1 = grep("PI/co-PI/Senior Personnel",t)
+  if(length(m1)==0) next;
+  
+  print(t[m1])
+  m1 = c(m1, length(t))
+  for(j in 1:(length(m1)-1)) {
+    q = t[m1[j]:m1[j+1]]
+    title =  gsub(".*?:(.*)","\\1",q[grep("sal Title",q)])
+    award =  gsub(".*?:(.*)","\\1",q[grep("Total Award Amount",q)]) 
+    if(length(title)>length(award)) { award=c(award,rep("n/a",length(title)-length(award))) }
+    startDate = gsub(".*?:(.*)","\\1",q[grep("al.*Start Date",q)])
+    endDate = gsub(".*?:(.*)","\\1",q[grep("End Date",q)])
+    if(length(title)>length(endDate)) { endDate=c(endDate,rep("n/a",length(title)-length(endDate))) }
+    
+    r = rbind(r,
+              data.frame(
+                proposal = f,
+                PI.name = t[m1[j]],
+                title,
+                award,
+                startDate =gsub(" ","",startDate),
+                endDate = gsub(" ","",endDate)
+              ))
+  }
+}
+# remove empty lines
+r=r[-which(gsub(" ","", gsub("\\$","", r$award))==""),]
+r$PI.name = gsub('.*?:(.*)','\\1',r$PI.name)
+format(as.Date(paste0("01/",r$startDate), tryFormats = c("%d/%m/%Y", "%d/%Y/%m")), format="%Y-%m")
+
+write.csv(r, file=file.path(path,'output.csv'), row.names = FALSE)
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # NSF-PendingSupport
- Extract and tabulize NSF current and pending support
+
+Extract and tabulize NSF current and pending support
+
+## Installation
+
+Configure path and run; if errors occur, then skip PDF file or modify. Output is saved in `output.csv`, which has the following structure:
+
+!(sample output for output.csv)[outputSample.png]
