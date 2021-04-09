d335723b71cc759a979348e9f7212dbc825bf13a
Thomas Gredig <thomas.gredig@csulb.edu>
2021-04-09 16:14:25 -0700

    fix end date

--- NSF-PendingSupport-Extractor.R
+++ NSF-PendingSupport-Extractor.R
@@ -13,17 +13,18 @@ library(pdftools)
 
 # find all the proposals
 file.list = dir(path, pattern='^NSF.*pdf$')
+print(paste("Found",length(file.list),"files."))
 
 # create output
 r = data.frame()
 for(f in file.list) {
   filename = file.path(path,f)
   print(filename)
-  t = pdf_text(filename) %>% readr::read_lines()
+  t = readr::read_lines(pdf_text(filename))
   m1 = grep("PI/co-PI/Senior Personnel",t)
   if(length(m1)==0) next;
   
-  print(t[m1])
+  print(t[m1]) # output PI/co-PI/personnel names
   m1 = c(m1, length(t))
   for(j in 1:(length(m1)-1)) {
     q = t[m1[j]:m1[j+1]]
@@ -48,6 +49,7 @@ for(f in file.list) {
 # remove empty lines
 r=r[-which(gsub(" ","", gsub("\\$","", r$award))==""),]
 r$PI.name = gsub('.*?:(.*)','\\1',r$PI.name)
-format(as.Date(paste0("01/",r$startDate), tryFormats = c("%d/%m/%Y", "%d/%Y/%m")), format="%Y-%m")
+r$startDate = format(as.Date(paste0("01/",r$startDate), tryFormats = c("%d/%m/%Y", "%d/%Y/%m")), format="%Y-%m")
+r$endDate = format(as.Date(paste0("01/",r$endDate), tryFormats = c("%d/%m/%Y", "%d/%Y/%m")), format="%Y-%m")
 
 write.csv(r, file=file.path(path,'output.csv'), row.names = FALSE)
